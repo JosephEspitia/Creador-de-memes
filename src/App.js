28d19a0d9db478b16de855ee2b9a106060ab366d
JosephEspitia <josephespitiac@gmail.com>
2022-03-26 19:10:04 -0500

    meme_app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import html2canvas from "html2canvas";
 
 function App() {
+  const [linea1, setLinea1] = useState("");
+  const [linea2, setLinea2] = useState("");
+  const [imag, setImg] = useState("");
+
+  const onChangeLinea1 = function (event) {
+    setLinea1(event.target.value);
+  };
+
+  const onChangeLinea2 = function (event) {
+    setLinea2(event.target.value);
+  };
+
+  const onChangeImg = function (event) {
+    setImg(event.target.value);
+  };
+
+  const exportMeme = function (event) {
+    html2canvas(document.querySelector("#meme")).then((canvas) => {
+      let imgen = canvas.toDataURL("image/png");
+      let link = document.createElement("a");
+      link.download = "meme.png";
+      link.href = imgen;
+      link.click();
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        onChange={onChangeLinea1}
+        type="text"
+        placeholder="Línea 1"
+      ></input>
+      <input
+        onChange={onChangeLinea2}
+        type="text"
+        placeholder="Línea 2"
+      ></input>
+
+      <select onChange={onChangeImg}>
+        <option value="fire">Casa en llamas</option>
+        <option value="futurama">Futurama</option>
+        <option value="history">History</option>
+        <option value="matrix">Matrix</option>
+        <option value="philosoraptor">Philosofalraptor</option>
+        <option value="smart">Smart Guy</option>
+      </select>
+
+      <button onClick={exportMeme}>Exportar</button>
+
+      <div className="meme" id="meme">
+        <span>{linea1}</span>
+        <span>{linea2}</span>
+        <img src={"/img/" + imag + ".jpg"} alt="meme" />
+      </div>
     </div>
   );
 }
